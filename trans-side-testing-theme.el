b3954e145b4cb7db5ae356f18963ab8847ef263e
kaushik.harith@gmail.com <kaushik.harith@gmail.com>
2020-02-25 10:46:09 -0500

    updated readme
    Signed-off-by: kaushik.harith@gmail.com <kaushik.harith@gmail.com>

--- trans-side-testing-theme.el
+++ trans-side-testing-theme.el
@@ -10,42 +10,36 @@
 		     ((((class color) (min-colors #xFFFFFF)))
 		      
 		      ;; Specify the color palette for each of the classes above.
+		      ;;basic bg/fg colors
 		      (bg-black      "#0D0D0D")
 		      (bg-dark       "#303030")
 		      (bg-gray       "#c6c6c6")
 		      (fg-white      "#FFFFFF")
 		      (fg-dark       "#5e5e5e")
 		      (fg-gray       "#919191")
+
+		      ;;darksn
 		      (pink-dark     "#a43261")
-		      (pink-light    "#ff81b8")
 		      (blue-dark     "#006ca5")
-		      (blue-light    "#3bd6ff")
 		      (aqua-dark     "#007086")
-		      (aqua-light    "#00ddf4")
 		      (purple-dark   "#6751a6")
-		      (purple-light  "#a38cc5")
 		      (magenta-dark  "#913e88")
-		      (magenta-light "#ffa7f6")
 		      (blue2-dark    "#0061b1")
+
+		      ;;lights
+		      (pink-light    "#ff81b8")
+		      (blue-light    "#3bd6ff")
+		      (aqua-light    "#00ddf4")
+		      (purple-light  "#a38cc5")
+		      (magenta-light "#ffa7f6")
 		      (blue2-light   "#93c9ff"))
 		     
 		     ;; specifications for Emacs faces.
-		     (
-		      (default
-			(:family "DejaVu Sans Mono" :foundry "PfEd" :width 'normal :height 165 :weight 'normal :slant 'normal :underline nil :overline nil :strike-through nil :box nil :inverse-video nil :foreground fg-white :background bg-black :stipple nil :inherit nil))
-		      (bold
-		       (:weight 'bold))
-		      (italic
-		       (:slant 'italic))
-		      (bold-italic
-		       (:weight 'bold :slant 'italic))
-		      (underline
-		       (:underline t))
-		      )
+		     ((default (:foreground fg-white :background bg-black)))
 		     
 		     ;; Forms after the face specifications are evaluated.
 		     ;; (palette vars can be used, read below for details.)
-		     (custom-theme-set-variables 'example-name
+		     (custom-theme-set-variables 'trans-side-testing
 						 `(ansi-color-names-vector [,bg-black
 									    ,bg-dark
 									    ,bg-gray
@@ -53,18 +47,23 @@
 									    ,fg-dark
 									    ,fg-gray
 									    ,pink-dark
-									    ,pink-light
 									    ,blue-dark
-									    ,blue-light
 									    ,aqua-dark
-									    ,aqua-light
 									    ,purple-dark
-									    ,purple-light
 									    ,magenta-dark
-									    ,magenta-light
 									    ,blue2-dark
+									    ,pink-light
+									    ,blue-light
+									    ,aqua-light
+									    ,purple-light
+									    ,magenta-light
 									    ,blue2-light])))
 
-      
+
+(and load-file-name
+     (boundp 'custom-theme-load-path)
+     (add-to-list 'custom-theme-load-path
+                  (file-name-as-directory
+                   (file-name-directory load-file-name))))
 
 (provide-theme 'trans-side-testing)
