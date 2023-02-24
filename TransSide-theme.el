4819988f19f27d733043d718964666b0abb99245
KAUSHIK SRINIVASAN <60790@archeangroup.dc>
2023-02-24 13:14:31 +0530

    License
    Added LICENSE file

--- TransSide-theme.el
+++ TransSide-theme.el
@@ -24,8 +24,6 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
-
 (require 'autothemer)
 
 (unless (>= emacs-major-version 24)
